5b5306af70d14ca9c0dd9314723c156743a61051
Ilyes Jayal <jayal.ilyes@gmail.com>
2023-08-28 21:56:59 +0200

    API init

---
+++ QA-back/Models/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QA_back.Models;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    { }
+
+    public DbSet<Api> Apis { get; set; }
+
+}
