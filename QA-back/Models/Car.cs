c39463eba5a2f65c6a56df680e7c600897baff5b
Ilyes Jayal <jayal.ilyes@gmail.com>
2023-08-28 22:47:51 +0200

    Ajout modeles bdd

---
+++ QA-back/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QA_back.Models;
+
+public class Car
+{
+    [Key]
+    public int idCar { get; set; }
+    public string? Immatriculation { get; set; }
+    public string? Type { get; set; }
+    public string? Marque { get; set; }
+    public string? Modele { get; set; }
+    public string? Couleur { get; set; }
+    [Column("Nombre de Portes")]
+    public int Nb_Portes { get; set; }
+    public int Disponibilité  { get; set; }
+    [Column("Nombre de Km")]
+    public int Nb_Km { get; set; }
+    public int key { get; set; }
+}
