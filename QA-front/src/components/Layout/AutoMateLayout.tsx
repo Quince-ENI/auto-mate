5fd2ee473e69ece898564f83eec6d96d4e737df7
CorentinCathe <50619005+CorentinCathe@users.noreply.github.com>
2023-08-29 12:14:46 +0200

    Revert "Ajout du footer"

--- QA-front/src/components/Layout/AutoMateLayout.tsx
+++ QA-front/src/components/Layout/AutoMateLayout.tsx
@@ -1,5 +1,4 @@
 import { Layout } from 'antd';
-import { Footer } from 'antd/es/layout/layout';
 import { Content } from 'antd/lib/layout/layout';
 import { FC, useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -10,7 +9,6 @@ import NavigationMenu from './NavigationMenu';
 
 const MainLayout = styled(Layout)`
   min-height: 100vh;
-  overflow: hidden;
 `;
 
 function useRedirectIfNotLogged(): void {
@@ -30,7 +28,6 @@ const AutoMateLayout: FC = () => {
       <Content>
         <Outlet />
       </Content>
-      <Footer style={{ backgroundColor: '#12551A', color: 'white' }}>Tous droits réservés © Quince 2022/2023</Footer>
     </MainLayout>
   );
 };
