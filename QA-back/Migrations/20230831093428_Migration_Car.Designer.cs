dcc7f76487baf3f3577e4f7d374da71d01f115cc
Ilyes Jayal <jayal.ilyes@gmail.com>
2023-08-31 12:12:44 +0200

    Migration & gitignore

--- QA-back/Migrations/20230831093428_Migration_Car.Designer.cs
+++ QA-back/Migrations/20230831093428_Migration_Car.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QA_back;
 
@@ -10,9 +11,11 @@ using QA_back;
 namespace QA_back.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230831093428_Migration_Car")]
+    partial class Migration_Car
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
